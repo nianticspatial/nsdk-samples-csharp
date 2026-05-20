97acde3c644ba0a8e54c474617ddb7fb0cff84d0
CI/CD Pipeline <ns-buildbot@nianticspatial.com>
2026-05-20 09:40:52 -0700

    NSDK Unity samples 4.1.0-26051913 - 2026-05-20 - Build #2539555456

---
+++ NsdkSamples/Assets/Samples/Common/Scripts/VersionRead.cs
@@ -0,0 +1,26 @@
+// Copyright 2022-2025 Niantic.
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class VersionRead : MonoBehaviour
+{
+
+    public Text uiTextBox;
+
+    private const string SamplesVersion = "4.1.0-26051913";
+
+    void Awake()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        uiTextBox.text = "SDK: " + NianticSpatial.NSDK.AR.Settings.Metadata.Version +
+            "\n" + "Samples: " + SamplesVersion;
+    }
+
+}
